45b4de59d36279abec683ee72d6b8697ac6fd48d
Azhar Arrozak <azhardrozak2001@upi.edu>
2025-06-15 20:04:46 +0700

    feat: add feature generate video with veo

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const { GoogleGenAI } = require("@google/genai");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
+const { Readable, finished } = require("stream");
 
 dotenv.config();
 
@@ -73,6 +74,59 @@ app.post("/imagen", async (req, res) => {
         res.status(500).json({ error: "Failed to generate image" });
     }
 });
+
+app.post("/veo", async (req, res) => {
+    const  { prompt } = req.body;
+
+    if (!prompt) {
+        return res.status(400).json({ error: "Prompt is required" });
+    }
+    try {
+        let operation = await ai.models.generateVideos({
+          model: "veo-2.0-generate-001",
+          prompt: prompt,
+          config: {
+            numberOfVideos: 1,
+            aspectRatio: "16:9",
+          },
+        });
+
+        console.log("Video generation operation started");
+
+        // Check if the operation is done
+        while (!operation.done) {
+          console.log("Waiting for video generation to complete...");
+          await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait for 5 seconds
+          operation = await ai.operations.getVideosOperation({
+            operation: operation,
+          });
+        }
+
+        console.log("Generated Video Success");
+
+        const videoBytes =
+          operation.response.generatedVideos[0].video.videoBytes;
+
+        if (!videoBytes) {
+          return res.status(500).json({ error: "No video generated" });
+        }
+
+        // Decode base64
+        const buffer = Buffer.from(videoBytes, "base64");
+        // Generate filename
+        const timestamp = Date.now();
+        const filename = `generated_video_${timestamp}.mp4`;
+        const filePath = path.join(__dirname, "public", filename);
+        // Save to file
+        fs.writeFileSync(filePath, buffer);
+
+        // Send video URL
+        res.json({ videoUrl: `/public/${filename}` });
+    } catch (error) {
+        console.error("Error generating video:", error);
+        res.status(500).json({ error: "Failed to generate video" });
+    }
+})
 
 
 app.listen(port, () => {
